0b7aefc5cba6b6dc398ad45e19bd93a728b45588
김태균 <bbac2422@gmail.com>
2023-04-21 22:29:20 +0900

    행렬의 곱셈
    문제 난이도 차체는 높지 않으나 행렬 곱샘을 까먹어 다시 공부함

---
+++ Lev 2/행렬의 곱셈.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Solution
+{
+    public int[,] solution(int[,] arr1, int[,] arr2)
+    {
+
+        int[,] answer = new int[arr1.GetLength(0), arr2.GetLength(1)];
+
+        //if(arr1.GetLength(1)==arr2.GetLength(0)) 행렬 필수조건
+
+        for (int i = 0; i < arr1.GetLength(0); i++)
+        {
+            for (int j = 0; j < arr1.GetLength(1); j++)
+            {
+                for (int k = 0; k < arr2.GetLength(1); k++)
+                {
+                    answer[i, k] += arr1[i, j] * arr2[j, k];
+                    //case 1 ex) [[1*3+4*3,1*3+4*3],[3*3+2*3,3*3+2*3],[4*3+1*3,4*3+1*3]]
+                }
+            }
+        }
+        return answer;
+    }
+}
